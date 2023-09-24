f4586c1515e96bcaa003a6c2e331948895fc9908
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-09-24 21:20:17 +0500

    complete auth and fix issue

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,38 +1,53 @@
 import React, { useState } from 'react'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../config/Firebasa';
 import { Link } from 'react-router-dom';
+import {  getFirestore,collection, addDoc } from 'firebase/firestore'; // Import Firestore functions
 import './page.css'
 
-const initiallizestate = { email: "", password: "" }
+const initialState = { name: "", email: "", password: "" };
+
 
 const Register = () => {
 
-  const [state, setState] = useState(initiallizestate)
+  const [state, setState] = useState(initialState)
 
   const handleChange = (e) => {
     setState({ ...state, [e.target.name]: e.target.value })
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const { email, password } = state;
+    const { email, password, name } = state;
   
-    
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-
-        console.log("User Registered")
-        console.log(userCredential)
-        console.log(user)
-    })
-    .catch((error) => {
-      console.error(error)
-      // ..
-    });
+    try {
+      // Create the user in Firebase Authentication
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+  
+      // Set the user's display name to be the email
+      await updateProfile(user, { displayName: email });
+  
+      // Store the user's data in Firestore
+      const db = getFirestore();
+      const usersRef = collection(db, 'users');
+  
+      await addDoc(usersRef, {
+        name: name,
+        email: email,
+        password: password,
+        // You can add more user-related data here if needed
+      });
+  
+      console.log("User Registered");
+      console.log(userCredential);
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
   };
   
+
+
 
 
 
@@ -47,84 +62,81 @@ const Register = () => {
           </div>
 
           <div className="mb-3">
-            <label
-              for="exampleInputEmail1"
-              className="form-label">
+            <label for="exampleInputName" className="form-label">
               Your Name
             </label>
             <input
               type="text"
-              name='name'
-              placeholder='First and Last name'
+              name="name"
+              placeholder="First and Last name"
               onChange={handleChange}
               className="form-control p-3"
-              id="exampleInputEmail1"
+              id="exampleInputName" // Update the input ID
               aria-describedby="emailHelp"
             />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
-          </div>
-          <div className="mb-3">
-            <label
-              for="exampleInputEmail1"
-              className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              name='email'
-              placeholder='Your email address'
-              onChange={handleChange}
-              className="form-control p-3"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div className="mb-3">
-            <label
-              for="exampleInputPassword1"
-              className="form-label"
-            >Password</label>
-            <input
-              type="password"
-              className="form-control p-3"
-              placeholder='Your password'
-              id="exampleInputPassword1"
-              name='password'
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-              onChange={handleChange} />
-            <label
-              className="form-check-label"
-              for="exampleCheck1"
-            > Check me out</label>
-          </div>
-          <div className='d-flex justify-content-center '>
 
-            <button
-              type="submit"
-              className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold "
-              onClick={handleSubmit}>
-              Submit
-            </button>
-          </div>
-
-          <p className='mt-3 text-center'>
-            Don't Have An Account?
-            <Link to='/SignIn'
-              className=' fw-bold text-decoration-none'>
-              Sign In</Link>
-          </p>
-        </form>
-
-
+          
       </div>
+      <div className="mb-3">
+        <label
+          for="exampleInputEmail1"
+          className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          name='email'
+          placeholder='Your email address'
+          onChange={handleChange}
+          className="form-control p-3"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="mb-3">
+        <label
+          for="exampleInputPassword1"
+          className="form-label"
+        >Password</label>
+        <input
+          type="password"
+          className="form-control p-3"
+          placeholder='Your password'
+          id="exampleInputPassword1"
+          name='password'
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+          onChange={handleChange} />
+        <label
+          className="form-check-label"
+          for="exampleCheck1"
+        > Check me out</label>
+      </div>
+      <div className='d-flex justify-content-center '>
+
+        <button
+          type="submit"
+          className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold "
+          onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+
+      <p className='mt-3 text-center'>
+        Already Have An Account?
+        <Link to='/SignIn'
+          className=' fw-bold text-decoration-none'>
+          Sign In</Link>
+      </p>
+    </form>
+
+
+      </div >
     </div >
   )
 }
--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,239 +1,178 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile, sendEmailVerification } from "firebase/auth";
 import { auth } from '../config/Firebasa';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFaceSmile } from '@fortawesome/free-solid-svg-icons';
-// import Register from './Register';
-// import {firebase} from 'firebase/app';
 
 const initiallizestate = { name: "", email: "", password: "" }
 
 const SignIn = () => {
-  // const provider = new GoogleAuthProvider();
-
-  const [state, setState] = useState(initiallizestate)
-  const [user, setUser] = useState("")
+  const [state, setState] = useState(initiallizestate);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        console.log(user)
-        setUser(user)
+        setUser(user);
       } else {
-        console.log('User is not signed in')
+        setUser(null);
       }
     });
 
-  }, [])
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(state)
+    const { email, password, name } = state;
 
-    const { email, password, name } = state
-
-    signInWithEmailAndPassword(auth, email, password, name )
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in 
         const user = userCredential.user;
+
+        // Set the display name to the name from the form
         updateProfile(user, { displayName: name })
-        .then(() => {
-          setUser(user);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+          .then(() => {
+            setUser(user);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       })
       .catch((error) => {
-        //   const errorCode = error.code;
-        //   const errorMessage = error.message;
-        console.log(error)
+        console.log(error);
       });
+  };
 
-  }
-//   const del = auth.currentUser
-//   const handleDelete = () => {
-//     deleteUser(del).then(() => {
-//       // User deleted.
-// console.log("User Deleted SuccessFully")
-//     }).catch((error) => {
-//       // An error ocurred
-//       // ...
-//     });
-//   }
-
-  const handleSignout = (e) => {
+  const handleSignout = () => {
     signOut(auth).then(() => {
-      console.log("User is Signed Out")
       window.location.reload();
-      // Sign Out SuccessFull
     }).catch((error) => {
-      console.log(error)
-    })
+      console.log(error);
+    });
+  };
 
-  }
-
-  // const usser = auth.currentUser;
-  // if(usser !== null){
-  //   const displayName = user.displayName;
-  //   const email = user.email;
-  //   const photoURL = user.photoURL;
-  //   const emailVerified = user.emailVerified;
-  // }
-  const handleEmailVerfication = () => {
+  const handleEmailVerification = () => {
     sendEmailVerification(auth.currentUser)
       .then(() => {
-        console.log('Email Verified')
-      })
-  }
-  const handleUpdateProfile = () => {
-    updateProfile(auth.currentUser, {
-      displayName: user.name, photoURL: "https://firebasestorage.googleapis.com/v0/b/blog-app-747b7.appspot.com/o/343492293_626551852662849_1932717340159949781_n.jpg?alt=media&token=a7eb8443-aeb4-4846-8805-e425f5c47a31",
-    }).then(() => {
-      window.location.reload();
-      // Profile updated!
-      // ...
-    }).catch((error) => {
-      // An error occurred
-      // ...
-      console.error(error)
-    });
-  }
-
-
+        console.log('Email Verified');
+      });
+  };
 
   return (
-
     <div>
-        {user.email
+      {user ? (
+        <div>
+          <img className='img-fluid rounded-5' src={user.photoURL} alt={`${user.displayName}`} />
+          <h2>User Email: {user.email}</h2>
+          <h2>User Name: {user.displayName}</h2>
+          <h2>UId : {user.uid}</h2>
 
+          <button
+            className='btn btn-danger fw-bold btn-sm'
+            onClick={handleSignout}>
+            Sign Out
+          </button>
+          <button
+            className='btn btn-danger fw-bold btn-sm'
+            onClick={handleEmailVerification}>
+            Verify Your Email
+          </button>
+        </div>
+      ) : (
+        <div className='d-flex justify-content-center flex-column'>
+          <h1 className='mt-3 mb-3 text-center text-danger'>Login</h1>
+          <form className='input-feild'>
+            <div className='fw-bold text-warning font-poppins text-center'>
+              <h5>Welcome Back</h5>
+              <FontAwesomeIcon className='fs-1' icon={faFaceSmile} />
+            </div>
 
-          ? <div>
-            <img className='img-fluid rounded-5'
-              src={user.photoURL} alt={`${user.email}`} />
-            <h2>User Email :{user.email}</h2>
-            <h2>User Name:  {user.displayName}</h2>
-            <button
-                className='btn btn-danger fw-bold btn-sm'
-                onClick={handleSignout}>
-                Sign Out
-              </button>
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
+                Your Name
+              </label>
+              <input
+                type="text"
+                name='name'
+                placeholder='First and Last name'
+                onChange={handleChange}
+                className="form-control p-3"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                name='email'
+                placeholder='Your email address'
+                onChange={handleChange}
+                className="form-control p-3"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control p-3"
+                placeholder='Your password'
+                id="exampleInputPassword1"
+                name='password'
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+                onChange={handleChange}
+              />
+              <label
+                className="form-check-label"
+                for="exampleCheck1"
+              >
+                Check me out
+              </label>
+            </div>
+            <div className='d-flex justify-content-center'>
               <button
-                className='btn btn-danger fw-bold btn-sm'
-                onClick={handleEmailVerfication}>
-                Verified Your
+                type="submit"
+                className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold"
+                onClick={handleSubmit}>
+                Submit
               </button>
-              <button
-                className='btn btn-danger fw-bold btn-sm'
-                onClick={handleUpdateProfile}>
-                Profile
-              </button>
-              {/* <button
-                className='btn btn-danger fw-bold btn-sm'
-                onClick={handleDelete}>
-               Delete User
-              </button> */}
-          </div>
+            </div>
+            <p className='mt-3 text-center'>
+              Don't Have An Account?
+              <Link to='/Register' className='fw-bold text-decoration-none'>
+                Register
+              </Link>
+            </p>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
 
-          : <div className='d-flex justify-content-center flex-column'>
-            < h1 className='mt-3 mb-3 text-center text-danger'>Login</h1>
-            <form className='input-feild'>
-              <div className='fw-bold text-warning font-poppins text-center'>
-
-                <h5 >Welcome Back</h5>
-                <FontAwesomeIcon className='fs-1' icon={faFaceSmile} />
-              </div>
-
-              <div className="mb-3">
-                <label
-                  for="exampleInputEmail1"
-                  className="form-label">
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  name='name'
-                  placeholder='First and Last name'
-                  onChange={handleChange}
-                  className="form-control p-3"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label
-                  for="exampleInputEmail1"
-                  className="form-label">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  name='email'
-                  placeholder='Your email address'
-                  onChange={handleChange}
-                  className="form-control p-3"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                />
-                <div id="emailHelp" className="form-text">
-                  We'll never share your email with anyone else.
-                </div>
-              </div>
-              <div className="mb-3">
-                <label
-                  for="exampleInputPassword1"
-                  className="form-label"
-                >Password</label>
-                <input
-                  type="password"
-                  className="form-control p-3"
-                  placeholder='Your password'
-                  id="exampleInputPassword1"
-                  name='password'
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-3 form-check">
-                <input type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                  onChange={handleChange} />
-                <label
-                  className="form-check-label"
-                  for="exampleCheck1"
-                > Check me out</label>
-              </div>
-              <div className='d-flex justify-content-center '>
-
-                <button
-                  type="submit"
-                  className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold  "
-                  onClick={handleSubmit}>
-                  Submit
-                </button>
-              </div>
-
-              <p className='mt-3 text-center'>
-                Don't Have An Account?
-                <Link to='/Register'
-                  className=' fw-bold text-decoration-none'>
-                  Register</Link>
-
-
-              </p>
-              
-            </form>
-            {/* <button onClick={deleteUser}>Delete User</button> */}
-            
-          </div>
-        }
-    </div >
-  )
-}
-
-export default SignIn
+export default SignIn;
