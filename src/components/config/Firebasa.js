23fa1fdf721a3322927c873d07e73010f0ab9bdd
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-09-27 23:52:28 +0500

    change config setting

--- src/components/config/Firebasa.js
+++ src/components/config/Firebasa.js
@@ -4,6 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage";
+import { getApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,6 +25,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const firestore = getFirestore(app)
-const storage = getStorage(app)
+const firebaseapp = getApp()
+const storage = getStorage(firebaseapp, "gs://blog-app-9bd8b.appspot.com")
 // const user = auth.currentUser;
 export {analytics, auth, app, firestore, storage}
