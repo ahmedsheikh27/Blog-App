f876c79681ee84d24bd63a751d9d131420b1d262
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-10-15 21:07:40 +0500

    auth complete

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -3,12 +3,19 @@ import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../config/Firebasa';
 import { Link } from 'react-router-dom';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../config/Firebasa';
 import './page.css';
 
-const initialState = { name: '', email: '', password: '', imageUrl: '' };
+const initialState = { name: '', email: '', password: '', imageFile: null };
 
 const Register = () => {
   const [state, setState] = useState(initialState);
+
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    setState({ ...state, imageFile: file });
+  };
 
   const handleChange = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
@@ -16,47 +23,53 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { email, password, name, imageUrl } = state;
+    const { email, password, name, imageFile } = state;
 
     try {
       // Create the user in Firebase Authentication
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
 
-      // Set the user's display name to be the name
-      await updateProfile(user, { displayName: name, photoURL:imageUrl });
+      // Upload the image file to Firebase Storage
+      if (imageFile) {
+        const imageRef = ref(storage, `images/${user.uid}/${imageFile.name}`);
+        await uploadBytes(imageRef, imageFile);
+        const imageUrl = await getDownloadURL(imageRef);
 
-      // Store the user's data in Firestore
-      const db = getFirestore();
-      const usersRef = collection(db, 'users');
+        // Set the user's display name and photo URL
+        await updateProfile(user, { displayName: name, photoURL: imageUrl });
 
-      await addDoc(usersRef, {
-        name: name,
-        email: email,
-        password:password,
-        imageUrl: imageUrl,
-        // You can add more user-related data here if needed
-      });
+        // Store the user's data in Firestore
+        const db = getFirestore();
+        const usersRef = collection(db, 'users');
+        await addDoc(usersRef, {
+          name: name,
+          email: email,
+          imageUrl: imageUrl,
+        });
 
-      console.log('User Registered');
-      console.log(userCredential);
-      console.log(user);
+        console.log('User Registered');
+        console.log(userCredential);
+        console.log(user);
+      } else {
+        console.error('Please select an image.');
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div>
+    <div className='input-feild'>
       <div className="d-flex justify-content-center flex-column">
         <h1 className="mt-3 text-center text-danger mb-3">Register</h1>
-        <form className='input-feild'>
+        <form className="input-field">
           <div className='fw-bold text-warning font-poppins text-center'>
-            <h5 >New to Here</h5>
+            <h5>New to Here</h5>
           </div>
 
           <div className="mb-3">
-            <label for="exampleInputName" className="form-label">
+            <label htmlFor="name" className="form-label">
               Your Name
             </label>
             <input
@@ -65,67 +78,67 @@ const Register = () => {
               placeholder="First and Last name"
               onChange={handleChange}
               className="form-control p-3"
-              id="exampleInputName" // Update the input ID
+              id="name"
               aria-describedby="emailHelp"
             />
+          </div>
 
-          
-      </div>
-      <div className="mb-3">
-        <label
-          for="exampleInputEmail1"
-          className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          name='email'
-          placeholder='Your email address'
-          onChange={handleChange}
-          className="form-control p-3"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div className="mb-3">
-        <label
-          for="exampleInputPassword1"
-          className="form-label"
-        >Password</label>
-        <input
-          type="password"
-          className="form-control p-3"
-          placeholder='Your password'
-          id="exampleInputPassword1"
-          name='password'
-          onChange={handleChange}
-        />
-      </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputImage" className="form-label">
+            <label
+              htmlFor="email"
+              className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              name='email'
+              placeholder='Your email address'
+              onChange={handleChange}
+              className="form-control p-3"
+              id="email"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label
+              htmlFor="password"
+              className="form-label"
+            >Password</label>
+            <input
+              type="password"
+              className="form-control p-3"
+              placeholder='Your password'
+              id="password"
+              name='password'
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="imageFile" className="form-label">
               Add Image
             </label>
             <input
               type="file"
               className="form-control p-3"
-              placeholder="Enter Profile Photo"
-              id="exampleInputImage"
-              name="imageUrl" // Change 'image' to 'imageUrl'
-              onChange={handleChange}
+              placeholder="Upload Profile Photo"
+              id="imageFile"
+              name="imageFile"
+              onChange={handleImage}
             />
           </div>
-          {/* ...other input fields */}
+
           <div className="d-flex justify-content-center">
             <button
               type="submit"
-              className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold "
+              className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold"
               onClick={handleSubmit}
             >
               Submit
             </button>
           </div>
 
-          <p className="mt-3 text-center">
+          <p className="mt-3 text-center text-sm">
             Already Have An Account?
             <Link to="/SignIn" className="fw-bold text-decoration-none">
               Sign In
--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile, sendEmailVerification } from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile, sendEmailVerification } from 'firebase/auth';
 import { auth } from '../config/Firebasa';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ListSubheader } from '@mui/material/List';
 import { faFaceSmile } from '@fortawesome/free-solid-svg-icons';
+import { Avatar, Card, CardContent, Typography, Button } from '@mui/material';
 
-const initiallizestate = { name: "", email: "", password: "", imageUrl:"" }
+import './page.css';
+
+const initialState = { name: '', email: '', password: '', imageUrl: '' };
 
 const SignIn = () => {
-  const [state, setState] = useState(initiallizestate);
+  const [state, setState] = useState(initialState);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -29,16 +33,14 @@ const SignIn = () => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSignIn = () => {
     const { email, password, name, imageUrl } = state;
 
-    signInWithEmailAndPassword(auth, email, password, name, imageUrl)
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-
-        // Set the display name to the name from the form
-        updateProfile(user, { displayName: name,imageUrl:user.imageUrl })
+        // Set the user's display name and photo URL
+        updateProfile(user, { displayName: name, photoURL: imageUrl })
           .then(() => {
             setUser(user);
           })
@@ -52,91 +54,97 @@ const SignIn = () => {
   };
 
   const handleSignout = () => {
-    signOut(auth).then(() => {
-      window.location.reload();
-    }).catch((error) => {
-      console.log(error);
-    });
-  };
-
-  const handleEmailVerification = () => {
-    sendEmailVerification(auth.currentUser)
+    signOut(auth)
       .then(() => {
-        console.log('Email Verified');
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
+
+  // const handleEmailVerification = () => {
+  //   sendEmailVerification(auth.currentUser)
+  //     .then(() => {
+  //       console.log('Email Verified');
+  //     });
+  // };
 
   return (
     <div>
       {user ? (
-        <div>
-          <img className='img-fluid rounded-5' src={user.imageUrl} alt={`${user.displayName}`} />
-          <h2>User Email: {user.email}</h2>
-          <h2>User Name: {user.displayName}</h2>
-          <h2>UId : {user.uid}</h2>
-
-          <button
-            className='btn btn-danger fw-bold btn-sm'
-            onClick={handleSignout}>
-            Sign Out
-          </button>
-          <button
-            className='btn btn-danger fw-bold btn-sm'
-            onClick={handleEmailVerification}>
-            Verify Your Email
-          </button>
-        </div>
+          <Card>
+          <CardContent>
+            <Avatar
+              alt={user.displayName}
+              src={user.photoURL}
+              sx={{ width: 100, height: 100, margin: '0 auto' }}
+            />
+            <Typography variant="h5" component="div" sx={{ textAlign: 'center', mt: 2 }}>
+              {user.displayName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" sx={{ textAlign: 'center' }}>
+              {user.email}
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSignout}
+              sx={{ display: 'block', margin: '20px auto' }}
+            >
+              Sign Out
+            </Button>
+          </CardContent>
+        </Card>
       ) : (
-        <div className='d-flex justify-content-center flex-column'>
-          <h1 className='mt-3 mb-3 text-center text-danger'>Login</h1>
-          <form className='input-feild'>
-            <div className='fw-bold text-warning font-poppins text-center'>
+        <div className="d-flex justify-content-center flex-column input-feild">
+          <h1 className="mt-3 mb-3 text-center text-danger">Login</h1>
+          <form className="input-field">
+            <div className="fw-bold text-warning font-poppins text-center">
               <h5>Welcome Back</h5>
-              <FontAwesomeIcon className='fs-1' icon={faFaceSmile} />
+              <FontAwesomeIcon className="fs-1" icon={faFaceSmile} />
             </div>
 
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">
+              <label htmlFor="name" className="form-label">
                 Your Name
               </label>
               <input
                 type="text"
-                name='name'
-                placeholder='First and Last name'
+                name="name"
+                placeholder="First and Last name"
                 onChange={handleChange}
                 className="form-control p-3"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                id="name"
               />
             </div>
 
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">
+              <label htmlFor="email" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
-                name='email'
-                placeholder='Your email address'
+                name="email"
+                placeholder="Your email address"
                 onChange={handleChange}
                 className="form-control p-3"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                id="email"
               />
-              <div id="emailHelp" className="form-text">
+              <div className="form-text">
                 We'll never share your email with anyone else.
               </div>
             </div>
             <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">
+              <label htmlFor="password" className="form-label">
                 Password
               </label>
               <input
                 type="password"
                 className="form-control p-3"
-                placeholder='Your password'
-                id="exampleInputPassword1"
-                name='password'
+                placeholder="Your password"
+                id="password"
+                name="password"
                 onChange={handleChange}
               />
             </div>
@@ -144,27 +152,25 @@ const SignIn = () => {
               <input
                 type="checkbox"
                 className="form-check-input"
-                id="exampleCheck1"
+                id="check"
                 onChange={handleChange}
               />
-              <label
-                className="form-check-label"
-                for="exampleCheck1"
-              >
+              <label className="form-check-label" htmlFor="check">
                 Check me out
               </label>
             </div>
-            <div className='d-flex justify-content-center'>
+            <div className="d-flex justify-content-center">
               <button
-                type="submit"
+                type="button"
                 className="btn btn-outline-warning d-flex justify-content-center text-dark fw-bold"
-                onClick={handleSubmit}>
+                onClick={handleSignIn}
+              >
                 Submit
               </button>
             </div>
-            <p className='mt-3 text-center'>
+            <p className="mt-3 text-center">
               Don't Have An Account?
-              <Link to='/Register' className='fw-bold text-decoration-none'>
+              <Link to="/Register" className="fw-bold text-decoration-none">
                 Register
               </Link>
             </p>
