2b3a487d660f80f38322f2958cfe0e27af135e6a
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-10-19 22:53:52 +0500

    add loader to app

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div className="camera-loader">
+    <FontAwesomeIcon icon={faCamera} className="camera-icon" spin />
+  </div>
+  )
+}
+
+export default Loader
