fda0b41e6cc51f2cf36a9d55c7bcbbc03cbd85b3
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-10-19 22:54:10 +0500

    set loader in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Navbar from './components/navbar/Navbar'
 import './App.css'
 import Footer from './components/footer/Footer'
+import Loader from './components/loader/Loader'
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
 
-  
-  
-  return (
+  useEffect(() => {
+    // Simulate async operation
+    setTimeout(() => {
+      setLoading(false); // Loading complete
+    }, 4000);
+  }, []);
+    return (
     <>
+    {
+      loading?
+      <Loader />:
+      <div>
       <Navbar />
       <Footer />
+      </div>
+    }
     </>
   );
       }
