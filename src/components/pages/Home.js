92ebedd95fd8baabac48c411511fd0b1e2f6cc91
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-11-10 01:20:47 +0500

    add new func

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,33 +4,45 @@ import blog from '../assets/blog.png'
 import HomeCard from '../card/HomeCard'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGreaterThan } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
+import { auth } from '../config/Firebasa'
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from 'react-router-dom'
 
 
 const Home = () => {
+  const navigate = useNavigate()
+  const getStarted = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('AddPost')
+      }
+      else {
+        navigate('SignIn')
+      }
+    })
+  }
 
   return (
     <>
-    <div>
+      <div>
 
-      <div class="card">
-        <img src={blog} alt="..." className='imag' />
-        <div class="card-content">
-          <h2>The Sustainable Investor</h2>
-          <h4>Making sustainability financially sound</h4>
-          <h5>Start your journey with us .</h5>
-          <Link to='/SignIn'>
-        <button className='start'>
-          Get Started 
-          <FontAwesomeIcon icon={faGreaterThan} />
-        </button>
-          </Link>
+        <div class="card">
+          <img src={blog} alt="..." className='imag' />
+          <div class="card-content">
+            <h2>The Sustainable Investor</h2>
+            <h4>Making sustainability financially sound</h4>
+            <h5>Start your journey with us .</h5>
+            <button className='start'
+            onClick={getStarted}>
+              Get Started
+              <FontAwesomeIcon icon={faGreaterThan} />
+            </button>
+          </div>
+        </div>
+        <div className='homecard'>
+          <HomeCard />
         </div>
       </div>
-<div className='homecard'>
-  <HomeCard />
-</div>
-    </div>
 
     </>
   )
