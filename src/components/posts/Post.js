0648c3c7fcead47dced1a71c77498844761f3340
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-11-12 00:35:41 +0500

    map time stamp

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -4,29 +4,24 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { CircularProgress, CardActionArea, Typography, Avatar } from '@mui/material';
+import { CardActionArea, Typography, Avatar } from '@mui/material';
 import Box from '@mui/material/Box';
 import ReactPaginate from 'react-paginate';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-// import Postcard from './Postcard'
-
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Loader from '../loader/Loader'
+import './Pagination.css'
 const Post = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
-  const [isLiked, setLiked] = useState(false);
-  // const [likeCount, setLikeCount] = useState(0);
 
-  const handleLikeToggle = () => {
-    setLiked((prevLikes) => !prevLikes);
-    // setLikeCount((prevCount) => (prevLikes ? prevCount - 1 : prevCount + 1));
-  };
+
 
   const postsPerPage = 9;
   const handlePageChange = ({ selected }) => {
@@ -70,7 +65,7 @@ const Post = () => {
       {loading ? (
         // Show loader while posts are loading
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <CircularProgress />
+          <Loader />
         </Box>
       ) : (
         <>
@@ -110,7 +105,7 @@ const Post = () => {
                         <Box sx={{
                           display: 'flex',
                           alignItems: 'center',
-                          background: '#f3f4f7',
+                          background: '#dee3e3',
                           padding: '5px'
                         }}>
                           <Avatar
@@ -154,7 +149,7 @@ const Post = () => {
                       )}
                       <CardMedia component="img" height="280" image={post.imageUrl} alt=""
                         sx={{ width: '300px' }} />
-                      <CardContent sx={{ background: '#f3f4f7' }}>
+                      <CardContent sx={{ background: '#dee3e3' }}>
                         {/* Display user profile and name */}
                         <Typography gutterBottom variant="h6" component="div">
                           {post.itemName}
@@ -164,10 +159,10 @@ const Post = () => {
                         </Typography>
 
                       </CardContent>
-                      <IconButton onClick={handleLikeToggle}>
-                        {isLiked ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
+                      {/* <IconButton onClick={() => handleLikeToggle(post.id)}>
+                        {isLiked[post.id] ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
                       </IconButton>
-                      {/* <Typography variant="body2">
+                      <Typography variant="body2">
                         {likeCount} {likeCount === 1 ? 'like' : 'likes'}
                       </Typography> */}
                     </CardActionArea>
@@ -180,8 +175,8 @@ const Post = () => {
             </Grid>
           </Box>
           <ReactPaginate
-            previousLabel={'previous'}
-            nextLabel={'next'}
+            previousLabel={'<'}
+            nextLabel={'>'}
             breakLabel={'...'}
             pageCount={Math.ceil(posts.length / postsPerPage)}
             marginPagesDisplayed={2}
@@ -190,6 +185,7 @@ const Post = () => {
             containerClassName={'pagination'}
             activeClassName={'active'}
           />
+
         </>
       )}
     </>
